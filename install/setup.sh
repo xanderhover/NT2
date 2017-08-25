29945f7f30ac9cccff84b033c8afd5e9682d9539
Xander Hover <x.hover@fontys.nl>
2017-08-25 13:55:14 +0200

    Added lib32 compat libs and wireshark to install

--- install/setup.sh
+++ install/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p ~/NT2
-cd ~/NT2
+mkdir -p $HOME/NT2
+cd $HOME/NT2
 
 #Fetch installer files
 if [ -e netkit-2.8.tar.bz2 ]
@@ -9,6 +9,7 @@ then
 	echo "File already downloaded!"
 else
 	wget http://wiki.netkit.org/download/netkit/netkit-2.8.tar.bz2
+	tar -xjSf netkit-2.8.tar.bz2
 fi
 
 if [ -e netkit-filesystem-i386-F5.2.tar.bz2 ]
@@ -16,6 +17,7 @@ then
 	echo "File already downloaded!"
 else	
 	wget http://wiki.netkit.org/download/netkit-filesystem/netkit-filesystem-i386-F5.2.tar.bz2
+	tar -xjSf netkit-kernel-i386-K2.8.tar.bz2
 fi
 
 if [ -e netkit-kernel-i386-K2.8.tar.bz2 ]
@@ -23,18 +25,15 @@ then
 	echo "File already downloaded!"
 else
 	wget http://wiki.netkit.org/download/netkit-kernel/netkit-kernel-i386-K2.8.tar.bz2
+	tar -xjSf netkit-filesystem-i386-F5.2.tar.bz2
 fi
 
+# experimental netkit images
 #wget http://wiki.netkit.org/download/netkit-filesystem/netkit-fs-sid-x86_64-experimental.bz2
 #wget http://wiki.netkit.org/download/netkit-kernel/netkit-kernel-4.0.0-x86_64-experimental.tar.bz2
 
 
 #unpack installer files
-
-tar -xjSf netkit-2.8.tar.bz2
-tar -xjSf netkit-kernel-i386-K2.8.tar.bz2
-tar -xjSf netkit-filesystem-i386-F5.2.tar.bz2
-
 #tar -xjSf netkit-fs-sid-x86_64-experimental.bz2
 #tar -xjSf netkit-kernel-4.0.0-x86_64-experimental.tar.bz2
 
@@ -46,14 +45,39 @@ cat << EOF >> ~/.bashrc
 alias vstart='vstart --append=root=98.0'
 alias lstart='lstart --pass=--append=root=98.0'
 
-export NETKIT_HOME=~/NT2/netkit
-export MANPATH=:$NETKIT_HOME/man
+export NETKIT_HOME=$HOME/NT2/netkit
+export MANPATH=$MANPATH:$NETKIT_HOME/man
 export PATH=$NETKIT_HOME/bin:$PATH
 
-. $NETKIT_HOME/bin/netkit_bash_completion
+source $NETKIT_HOME/bin/netkit_bash_completion
 
 #TIKTEN
 
 EOF
+
+# during the installation we also need to check the configation
+
+alias vstart='vstart --append=root=98.0'
+alias lstart='lstart --pass=--append=root=98.0'
+
+export NETKIT_HOME=$HOME/NT2/netkit
+export MANPATH=$MANPATH:$NETKIT_HOME/man
+export PATH=$NETKIT_HOME/bin:$PATH
+
+if [ `uname -i` == 'x86_64' ]
+then
+	apt install ia32-libs
+	apt install libc6-i386
+fi
+
+if [ `which xterm`.x == .x ]
+then
+	apt install xterm
+fi
+
+if [ `which wireshark`.x == .x ]
+then
+	apt install wireshark
+fi
 
 
